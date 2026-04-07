fa95a418e98ba7b7669778fe2e2126bd3a61fd15
gksahoo2003-dev <gksahoo2003@gmail.com>
2026-04-07 13:02:23 +0530

    first commit

--- server.js
+++ server.js
@@ -7,32 +7,34 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// DB connect
 mongoose.connect("mongodb://127.0.0.1:27017/disasterDB")
 .then(()=> console.log("DB Connected"))
 .catch(err=> console.log(err));
 
-// test route
+const disasterSchema = new mongoose.Schema({
+    type: String,
+    location: String,
+    description: String,
+    date: { type: Date, default: Date.now }
+});
+
+const Disaster = mongoose.model("Disaster", disasterSchema);
+
 app.get("/", (req, res) => {
-    res.send("Server is running");
+    res.send("Server running");
+});
+
+app.post("/add", async (req, res) => {
+    const data = new Disaster(req.body);
+    await data.save();
+    res.send("Data Saved");
+});
+
+app.get("/all", async (req, res) => {
+    const data = await Disaster.find();
+    res.json(data);
 });
 
 app.listen(3000, () => {
     console.log("Server started at port 3000");
-});
-
-
-const Disaster = require("./models/Disaster");
-
-// Add Disaster
-app.post("/add", async (req, res) => {
-    const data = new Disaster(req.body);
-    await data.save();
-    res.send("Disaster Data Saved");
-});
-
-// Get All Data
-app.get("/all", async (req, res) => {
-    const data = await Disaster.find();
-    res.json(data);
 });
